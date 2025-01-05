52c7bf0521b9024ebfc794ad5c49fc4334703844
Tamim Ahmed <tamim20072@gmail.com>
2025-01-05 13:15:38 +0600

    some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Tamim's's Portfolio",
+  description: "Modern & Minimal Tamim Portfolio",
 };
 
 export default function RootLayout({
