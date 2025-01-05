b1c1e6d43dab246362189131c4547ba091f16553
Tamim Ahmed <tamim20072@gmail.com>
2025-01-05 13:16:24 +0600

    Web title edited

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tamim's's Portfolio",
+  title: "Tamim's Portfolio",
   description: "Modern & Minimal Tamim Portfolio",
 };
 
